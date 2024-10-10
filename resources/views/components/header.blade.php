c7d1722f2c4c69287d14708d3db79aea2ef433b3
Syarif Muhammad Alhaiza <alqadrihaiza@gmail.com>
2024-10-10 21:01:35 +0700

    Make Route To Login Page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">TodoApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="btn btn-primary" aria-current="page" href="{{ route('login') }}">Login</a>
+            </div>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,22 +10,7 @@
 </head>
 
 <body class="">
-
-    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">TodoApp</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ms-auto">
-                    <a class="btn btn-primary" aria-current="page" href="#">Login</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
+    @include('components.header')
 
     @yield('container')
 
